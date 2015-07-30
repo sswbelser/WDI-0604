1a6c148bb1f11bd308a482165ef05867db6f0915
Sabastian <sswbelser@gmail.com>
2015-07-30 14:56:24 -0700

    Edits/updates all work

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -9,10 +9,55 @@ class PlanesController < ApplicationController
 		render :new
 	end
 
+	# POST /planes
 	def create
-		plane = params.require(:plane).permit(:name, :design, :description)
-		Plane.create(plane)
-		redirect_to "/planes"
+		# new plane data from form
+		plane_params = params.require(:plane).permit(:name, :design, :description)
+
+		# create new plane in db
+		plane = Plane.create(plane_params)
+
+		# redirect to plane's show page
+		redirect_to "/planes/#{plane.id}"
 	end
 
+	# GET /planes/:id
+	def show
+		# set id from url params
+		plane_id = params[:id]
+
+		# find plane in db by its id
+		@plane = Plane.find(plane_id)
+
+		# render show view
+		render :show
+	end
+
+	def edit
+		# set id from url params
+		plane_id = params[:id]
+
+		# find plane in db by its id
+		@plane = Plane.find(plane_id)
+
+		render :edit
+	end
+
+	# PUT /planes/:id
+	def update
+		# set id from url params
+		plane_id = params[:id]
+
+		# find plane in db by its id
+		db_plane = Plane.find(plane_id)
+
+		# updated plane data from form
+		plane_params = params.require(:plane).permit(:name, :design, :description)
+
+		# update the plane in db
+		db_plane.update_attributes(plane_params)
+
+		# redirect to plane's show page
+		redirect_to "/planes/#{db_plane.id}"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-    root to: 'planes#index'
+  # homepage ("/") goes to planes controller, index action
+  root to: 'planes#index'  # localhost:3000/planes
 
-    resources :planes, only: [:index, :new, :create]
+  # CRUD routes for planes
+  resources :planes
 end
 
-#    Prefix Verb URI Pattern           Controller#Action
-#      root GET  /                     planes#index
-#    planes GET  /planes(.:format)     planes#index
-#           POST /planes(.:format)     planes#create
-# new_plane GET  /planes/new(.:format) planes#new
+#     Prefix Verb   URI Pattern                Controller#Action
+#       root GET    /                          planes#index
+#     planes GET    /planes(.:format)          planes#index
+#            POST   /planes(.:format)          planes#create
+#  new_plane GET    /planes/new(.:format)      planes#new
+# edit_plane GET    /planes/:id/edit(.:format) planes#edit
+#      plane GET    /planes/:id(.:format)      planes#show
+#            PATCH  /planes/:id(.:format)      planes#update
+#            PUT    /planes/:id(.:format)      planes#update
+#            DELETE /planes/:id(.:format)      planes#destroy
